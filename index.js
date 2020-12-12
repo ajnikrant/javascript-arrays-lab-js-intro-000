f12913debac9fea1c3bc12d93313df2c44c0ec5c
Amanda Nikrant <ajnikrant@gmail.com>
2020-12-12 22:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+console.log(kittens)
+
+function destructivelyAppendKitten(){
+kittens.push("Ralph");
+}
+  console.log(kittens);
+
+function destructivelyPrependKitten(){
+  kittens.unshift("Bob");
+}
+  console.log(kittens);
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+console.log(kittens);
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+console.log(kittens);
+
+
+function appendKitten() {
+return [...kittens, "Broom"];
+}
+console.log(appendKitten());
+
+function prependKitten() {
+  return ["Arnold", ...kittens];
+}
+console.log(prependKitten());
